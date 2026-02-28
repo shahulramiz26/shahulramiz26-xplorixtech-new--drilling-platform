1268d8df13eea0631f6fdcdc5ab515288cb9da70
shahulramiz26 <shahulramiz26@gmail.com>
2026-02-28 18:23:43 +0530

    Configure Next.js for static export deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
