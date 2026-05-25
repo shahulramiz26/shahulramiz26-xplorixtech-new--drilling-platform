ef734ed2b65e445ec6ea3f4aa260fc933f53ee07
shahulramiz26 <shahulramiz26@gmail.com>
2026-05-25 18:44:54 +0530

    Update page.tsx

--- app/admin/finance/master-data/page.tsx
+++ app/admin/finance/master-data/page.tsx
@@ -359,8 +359,4 @@ export default function MasterDataPage() {
   )
 }
 
-// Missing icon
-function Save({ size, ...props }: any) {
-  return <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" {...props}><path d="M19 21H5a2 2 0 01-2-2V5a2 2 0 012-2h11l5 5v11a2 2 0 01-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/></svg>
-}
 
